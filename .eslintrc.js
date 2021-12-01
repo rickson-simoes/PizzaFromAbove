6f8ecf410166e618bd90734d46dd022632d97997
Rickson <rickson.simoes@hotmail.com>
2021-12-01 15:01:56 -0300

    :wrench: Repairs

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,14 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-components': 'warn',
+    'max-len': 'off',
+    'no-plusplus': 'off',
+    'default-case': 'off',
+    'no-return-assign': 'off',
+    'import/extensions': 'off',
+    'no-param-reassign': 'off',
+    'no-alert': 'warn',
+    'no-unused-vars': 'warn'
   },
 };
